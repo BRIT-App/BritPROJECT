ddf87a6f5ec0ef6a5f913d7e1009b239ba1b9d31
ShafinMohammad <65834265+ShafinMohammad@users.noreply.github.com>
2020-05-24 01:00:27 -0600

    added fragments and splash screen

--- app/src/main/java/com/example/britapp/MainActivity.java
+++ app/src/main/java/com/example/britapp/MainActivity.java
@@ -1,29 +1,51 @@
-package com.example.britapp;
+package com.britcoders.splashscreen;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.example.britapp.AppBarConfiguration;
+import com.example.britapp.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mGreeting;
-    private Button mGoodbyeButton;
+    //Variables
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
 
-        mGoodbyeButton = (Button) findViewById(R.id.goodbye_button);
-        mGreeting = (TextView) findViewById(R.id.greeting);
-        mGoodbyeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mGoodbyeButton.setVisibility(View.INVISIBLE);
-                mGreeting.setVisibility(View.INVISIBLE);
-            }
-        });
+        //Animation
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        //Hooks
+        image = findViewByID(R.id.imageView);
+        logo = findViewByID(R.id.textView);
+        slogan = findViewByID(R.id.textView2);
+
     }
+
 }
---
+++ app/src/main/java/com/example/britapp/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,4 @@
+package com.example.britapp.ui.dashboard;
+
+public class DashboardFragment {
+}
