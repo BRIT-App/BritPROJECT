58315ac609e18acba817a0e55a11e26b141a6ef7
ShafinMohammad <65834265+ShafinMohammad@users.noreply.github.com>
2020-05-25 14:23:30 -0600

    splashscreen works but the noActionBar causes app to crash

--- app/src/main/java/com/britcoders/britapp/MainActivity.java
+++ app/src/main/java/com/britcoders/britapp/MainActivity.java
@@ -1,13 +1,6 @@
 package com.britcoders.britapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -19,17 +12,9 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIMER = 5000;
-
-    //Variables
-    Animation topAnim, bottomAnim;
-    ImageView backgroundImage;
-    TextView title, poweredByLine;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -40,30 +25,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
-        //Animation
-        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
-        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
-
-        //Hooks
-        backgroundImage = findViewById(R.id.background_image);
-        title = findViewById(R.id.title);
-        poweredByLine = findViewById(R.id.powered_by_line);
-
-        backgroundImage.setAnimation(topAnim);
-        title.setAnimation(bottomAnim);
-        poweredByLine.setAnimation(bottomAnim);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run(){
-
-            Intent intent = new Intent(getApplicationContext(), UserDashboard.class);
-            startActivity(intent);
-            finish(); //destroy activity after running
-            //don not write anything else here
-            }
-        },SPLASH_TIMER);
-
     }
+
 }
--- app/src/main/java/com/britcoders/britapp/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/britcoders/britapp/ui/notifications/NotificationsFragment.java
@@ -1,4 +1,4 @@
-package com.britcoders.splashscreen.ui.notifications;
+package com.britcoders.britapp.ui.notifications;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -16,12 +16,12 @@ import com.britcoders.britapp.R;
 
 public class NotificationsFragment extends Fragment {
 
-    private com.britcoders.splashscreen.ui.notifications.NotificationsViewModel notificationsViewModel;
+    private NotificationsViewModel notificationsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
-                ViewModelProviders.of(this).get(com.britcoders.splashscreen.ui.notifications.NotificationsViewModel.class);
+                ViewModelProviders.of(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
